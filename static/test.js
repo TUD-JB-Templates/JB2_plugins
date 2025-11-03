b7295f786014c0b00cb954062198131c521f6844
luukfroling <L.Froling@student.tudelft.nl>
2025-11-03 14:16:20 +1100

    Update test.js

--- static/test.js
+++ static/test.js
@@ -1,24 +1,25 @@
 // log hello world once document is loaded
 document.addEventListener("DOMContentLoaded", function() {
     console.log("Hello, World!");
+    (function () {
+    const script = document.createElement("script");
+    script.src = "https://giscus.app/client.js";
+    script.setAttribute("data-repo", "TUD-JB-Templates/JB2_plugins");
+    script.setAttribute("data-repo-id", "R_kgDOPrPv2Q");
+    script.setAttribute("data-category", "Page comments");
+    script.setAttribute("data-category-id", "DIC_kwDOPrPv2c4CxX_-");
+    script.setAttribute("data-mapping", "pathname");
+    script.setAttribute("data-strict", "0");
+    script.setAttribute("data-reactions-enabled", "1");
+    script.setAttribute("data-emit-metadata", "0");
+    script.setAttribute("data-input-position", "top");
+    script.setAttribute("data-theme", "preferred_color_scheme");
+    script.setAttribute("data-lang", "en");
+    script.crossOrigin = "anonymous";
+    script.async = true;
+    document.head.appendChild(script);
+    console.log("Giscus script added to the document.");
+    })();
 });
 
-(function () {
-  const script = document.createElement("script");
-  script.src = "https://giscus.app/client.js";
-  script.setAttribute("data-repo", "TUD-JB-Templates/JB2_plugins");
-  script.setAttribute("data-repo-id", "R_kgDOPrPv2Q");
-  script.setAttribute("data-category", "Page comments");
-  script.setAttribute("data-category-id", "DIC_kwDOPrPv2c4CxX_-");
-  script.setAttribute("data-mapping", "pathname");
-  script.setAttribute("data-strict", "0");
-  script.setAttribute("data-reactions-enabled", "1");
-  script.setAttribute("data-emit-metadata", "0");
-  script.setAttribute("data-input-position", "top");
-  script.setAttribute("data-theme", "preferred_color_scheme");
-  script.setAttribute("data-lang", "en");
-  script.crossOrigin = "anonymous";
-  script.async = true;
-  document.head.appendChild(script);
-  console.log("Giscus script added to the document.");
-})();
+
