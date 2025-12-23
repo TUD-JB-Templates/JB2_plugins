da62039778dd91c5347096583de96c3120e9926f
luukfroling <L.Froling@student.tudelft.nl>
2025-12-23 17:36:51 +1100

    Update wizard-injector-2000.js

--- static/wizard-injector-2000.js
+++ static/wizard-injector-2000.js
@@ -1,12 +1,2 @@
 console.log("Wizard Injector 2000 active.");
-
-const queryString = window.location.search;
-
-// Create a URLSearchParams object
-const urlParams = new URLSearchParams(queryString);
-
-// Get the value of a specific parameter
-const isEditor = urlParams.get('editor');
-
-console.log(urlParams)
-console.log(`Editor parameter value: ${isEditor}`);
+console.log("url contains 'editor' =", window.location.href.includes("editor"));
