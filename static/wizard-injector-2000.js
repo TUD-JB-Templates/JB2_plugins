0bf8adb00fb3841ee95d4fb13127281819e0247c
luukfroling <L.Froling@student.tudelft.nl>
2025-12-23 17:33:03 +1100

    parameter logic

--- static/wizard-injector-2000.js
+++ static/wizard-injector-2000.js
@@ -1,1 +1,11 @@
 console.log("Wizard Injector 2000 active.");
+
+const queryString = window.location.search;
+
+// Create a URLSearchParams object
+const urlParams = new URLSearchParams(queryString);
+
+// Get the value of a specific parameter
+const isEditor = urlParams.get('editor');
+
+console.log(`Editor parameter value: ${isEditor}`);
