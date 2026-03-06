c0e38bcaf4df63faaa38af88747dad8f7d8cf6bf
luukfroling <luukfroling@gmail.com>
2026-03-06 17:39:55 +1100

    add wizard via widgets

---
+++ plugins/wizard.mjs
@@ -0,0 +1,27 @@
+const plugin = {
+  name: 'Append anywidget to every page',
+  transforms: [
+    {
+      name: 'transform-add-anywidget',
+      doc: 'Adds an anywidget to the end of every page',
+      stage: 'document',
+      plugin: (_, utils) => (node) => {
+        const anyWidgetNode = {
+            type: 'block',
+            children: [
+                {
+                type: 'anywidget',
+                esm: './widgets/wizard.mjs',
+                model: {},
+                css: undefined,
+                class: undefined
+                }
+            ]
+        }
+        node.children.push(anyWidgetNode);
+      },
+    },
+  ],
+};
+
+export default plugin;
