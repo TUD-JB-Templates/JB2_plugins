c769a2ab02ca7cafc02194b34dcecaaf1826e866
luukfroling <luukfroling@gmail.com>
2026-03-11 17:26:46 +1100

    Update giscus.mjs

--- plugins/giscus.mjs
+++ plugins/giscus.mjs
@@ -11,7 +11,7 @@ const plugin = {
             children: [
                 {
                 type: 'anywidget',
-                esm: 'widgets/giscus.mjs',
+                esm: 'https://github.com/TUD-JB-Templates/JB2_plugins/releases/download/giscus-widget/giscus.mjs',
                 model: {},
                 css: undefined,
                 class: undefined
